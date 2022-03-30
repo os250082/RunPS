72af1438575ddafb33851660903c6c4a49cb1d18
os250082 <oz.shpigel@ncr.com>
2022-03-30 18:18:52 +0300

    update

---
+++ PSTest/PSTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PSTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunPS(@".\test.ps1", "");
+        }
+
+        private static void RunPS(string scriptName, string scriptParameters)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"powershell.exe";
+
+            startInfo.Arguments = scriptName +
+                                  " " + scriptParameters;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+            var output = process.StandardOutput.ReadToEnd();
+            var errors = process.StandardError.ReadToEnd();
+
+            if (!string.IsNullOrEmpty(errors))
+                throw (new Exception(errors));
+        }
+    }
+}
